8e1d9efa60535dcbb135571b005aecbdf9f93054
Bung Danar <danar.g.putera@gmail.com>
2025-10-03 18:21:41 +0700

    Finish module

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  output,
+  viewChild,
+  ViewChild,
+} from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
+import { ControlComponent } from '../../../shared/control/control.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, ControlComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css',
 })
-export class NewTicketComponent {}
+export class NewTicketComponent {
+  // @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
+  private form = viewChild<ElementRef<HTMLFormElement>>('form');
+
+  enteredTitle = '';
+  enteredText = '';
+  add = output<{ title: string; text: string }>();
+
+  onSubmit() {
+    this.add.emit({
+      title: this.enteredTitle,
+      text: this.enteredText,
+    });
+
+    this.enteredTitle = '';
+    this.enteredText = '';
+  }
+}
--- src/app/dashboard/tickets/ticket/ticket.component.ts
+++ src/app/dashboard/tickets/ticket/ticket.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, input, output, signal } from '@angular/core';
+import { Ticket } from '../ticket.model';
 
 @Component({
   selector: 'app-ticket',
   standalone: true,
   imports: [],
   templateUrl: './ticket.component.html',
-  styleUrl: './ticket.component.css'
+  styleUrl: './ticket.component.css',
 })
 export class TicketComponent {
+  data = input.required<Ticket>();
+  detailsVisible = signal(false);
+  close = output();
 
+  onToggleDetails() {
+    // this.detailsVisible.set(!this.detailsVisible());
+    this.detailsVisible.update((oldValue) => {
+      return !oldValue;
+    });
+  }
+
+  onMarkAsCompleted() {
+    this.close.emit();
+  }
 }
